a11920af4614e165a13230684322839dbde8cd9e
Junjie Li <jjtommyliii@gmail.com>
2020-08-27 14:17:45 +0200

    Exercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StringDuplicatorTest {
+    
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String stringDuplicate = SUT.duplicate("");
+        assertThat(stringDuplicate, is(""));
+    }
+
+    @Test
+    public void duplicate_simpleString_duplicatedStringReturned() {
+        String stringDuplicate = SUT.duplicate("a");
+        assertThat(stringDuplicate, is("aa"));
+    }
+
+    @Test
+    public void duplicate_longString_duplicatedStringReturned() {
+        String stringDuplicate = SUT.duplicate("Junjie");
+        assertThat(stringDuplicate, is("JunjieJunjie"));
+
+    }
 }
